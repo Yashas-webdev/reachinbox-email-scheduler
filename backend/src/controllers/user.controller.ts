9b43fcbb8903a3f3b092ed7e61ffdb4b8f20d884
Yashas-webdev <yashasaryashu@gmail.com>
2026-08-25 20:39:00 +0530

    created controller

---
+++ backend/src/controllers/user.controller.ts
@@ -0,0 +1,40 @@
+import type { Request, Response } from "express";
+import { prisma } from "../config/prisma.js";
+
+export const createuser = async(
+    req: Request,
+    res: Response
+) => {
+    try{
+        const {googleId, name, email, avatar}= req.body;
+
+        if(!googleId || !name || !email){
+            return res.status(400).json({
+                message: "googleId, name and email are require",
+            });
+          }
+
+          const user = await prisma.user.create({
+            data: {
+                googleId,
+                name,
+                email,
+                avatar,
+            },
+          });
+
+          return res.status(201).json({
+            message: "User created successfully",
+            user,
+          });
+        
+    } catch (error) {
+        console.error("Create user error",error);
+
+        return res.status(500).json({
+            message: "Failed to create user",
+        })
+
+    }
+
+};
